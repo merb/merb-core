5562b7df77f01f1554aad9907eb69fc4f4a5e1f9
snusnu <gamsnjaga@gmail.com>
2009-11-04 06:49:14 +0100

    [merb-core] Bumped version to 1.0.15 and updated CHANGELOG

--- lib/merb-core/version.rb
+++ lib/merb-core/version.rb
@@ -1,5 +1,5 @@
 module Merb
-  VERSION    = '1.0.14' unless defined?(Merb::VERSION)
-  DM_VERSION = '0.10' unless defined?(Merb::DM_VERSION)
-  DO_VERSION = '0.10' unless defined?(Merb::DO_VERSION)
+  VERSION    = '1.0.15' unless defined?(Merb::VERSION)
+  DM_VERSION = '0.10'   unless defined?(Merb::DM_VERSION)
+  DO_VERSION = '0.10'   unless defined?(Merb::DO_VERSION)
 end
