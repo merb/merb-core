1838d2fc545859630b14738404a82d0836af1d26
Nicos Gollan <gtdev@spearhead.de>
2011-04-07 16:55:06 +0200

    [merb-core] Mark some multipart specs pending
    The spec code needs to make proper use of webrat.

--- spec/public/test/20_multipart_request_helper_spec.rb
+++ spec/public/test/20_multipart_request_helper_spec.rb
@@ -43,59 +43,62 @@ describe Merb::Test::MultipartRequestHelper do
   end
 
   describe "#multipart_post" do
-    before(:each) do
-      Merb::Router.prepare do
-        resources :spec_helper_controller
+    pending "needs proper spec code (Webrat)" do
+      before(:each) do
+        Merb::Router.prepare do
+          resources :spec_helper_controller
+        end
+      end
+
+      it "should post to the create action with params" do
+        resp = multipart_post("/spec_helper_controller", :name => "Harry")
+        JSON(resp.body.to_s)["name"].should == "Harry"
+      end
+
+      it "should upload a file to the action using multipart" do
+        file_name = File.join(File.dirname(__FILE__), "multipart_upload_text_file.txt")
+        File.open( file_name ) do |file|
+          resp = multipart_post("/spec_helper_controller", :my_file => file)
+          file_params = JSON(resp.body.to_s)["my_file"]
+          file_params["content_type"].should == "text/plain"
+          file_params["size"].should == File.size(file_name)
+          file_params["tempfile"].should =~ /#<File:.*>/
+            file_params["filename"].should == "multipart_upload_text_file.txt"
+        end
       end
     end
-  
-    it "should post to the create action with params" do
-      resp = multipart_post("/spec_helper_controller", :name => "Harry")
-      JSON(resp.body.to_s)["name"].should == "Harry"
-    end
-  
-    it "should upload a file to the action using multipart" do
-      file_name = File.join(File.dirname(__FILE__), "multipart_upload_text_file.txt")
-      File.open( file_name ) do |file|
-        resp = multipart_post("/spec_helper_controller", :my_file => file)
-        file_params = JSON(resp.body.to_s)["my_file"]
-        file_params["content_type"].should == "text/plain"
-        file_params["size"].should == File.size(file_name)
-        file_params["tempfile"].should =~ /#<File:.*>/
-        file_params["filename"].should == "multipart_upload_text_file.txt"
-      end
-    end
-  
   end
 
   describe "#multipart_put" do
-    before(:each) do
-      Merb::Router.prepare do
-        resources :spec_helper_controller
+    pending "needs proper spec code (Webrat)" do
+      before(:each) do
+        Merb::Router.prepare do
+          resources :spec_helper_controller
+        end
       end
-    end
-  
-    it "should put to the update action with multipart params" do
-      Merb::Test::ControllerAssertionMock.should_receive(:called).with(:update)
-      resp = multipart_put("/spec_helper_controller/my_id", :name => "Harry")
-      
-      params = JSON(resp.body.to_s).to_mash
-      params[:name].should == "Harry"
-      params[:id].should   == "my_id"
-    end
-  
-    it "should upload a file to the action using multipart" do
-      Merb::Test::ControllerAssertionMock.should_receive(:called).with(:update)
-      file_name = File.join(File.dirname(__FILE__), "multipart_upload_text_file.txt")
-      File.open( file_name ) do |file|
-        resp = multipart_put("/spec_helper_controller/my_id", :my_file => file)
+
+      it "should put to the update action with multipart params" do
+        Merb::Test::ControllerAssertionMock.should_receive(:called).with(:update)
+        resp = multipart_put("/spec_helper_controller/my_id", :name => "Harry")
+
         params = JSON(resp.body.to_s).to_mash
-        params[:id].should == "my_id"
-        file_params = params[:my_file]
-        file_params[:content_type].should == "text/plain"
-        file_params[:size].should == File.size(file_name)
-        file_params[:tempfile].should =~ /#<File:.*>/
-        file_params[:filename].should == "multipart_upload_text_file.txt"
+        params[:name].should == "Harry"
+        params[:id].should   == "my_id"
+      end
+
+      it "should upload a file to the action using multipart" do
+        Merb::Test::ControllerAssertionMock.should_receive(:called).with(:update)
+        file_name = File.join(File.dirname(__FILE__), "multipart_upload_text_file.txt")
+        File.open( file_name ) do |file|
+          resp = multipart_put("/spec_helper_controller/my_id", :my_file => file)
+          params = JSON(resp.body.to_s).to_mash
+          params[:id].should == "my_id"
+          file_params = params[:my_file]
+          file_params[:content_type].should == "text/plain"
+          file_params[:size].should == File.size(file_name)
+          file_params[:tempfile].should =~ /#<File:.*>/
+            file_params[:filename].should == "multipart_upload_text_file.txt"
+        end
       end
     end
   end
@@ -126,7 +129,7 @@ module Merb::Test::MultipartRequestHelper
       Param.should_receive(:new).with('normal', 'normal_param').and_return(@fake_return_param)
       Post.new.push_params(params)
     end
-  
+
     it "should create FileParam from params when param does response to read" do
       file_param = mock('file param')
       file_param.should_receive(:read).and_return('file contents')
@@ -136,7 +139,7 @@ module Merb::Test::MultipartRequestHelper
       Post.new.push_params(params)
     end
   end
-  
+
   describe Post, '#to_multipart' do
     it "should create a multipart request from the params" do
       file_param = mock('file param')
